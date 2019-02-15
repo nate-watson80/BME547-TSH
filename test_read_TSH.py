f81405b1216207d476e242b0a65ee52b7bd0a773
Nate Watson <nate.watson@duke.edu>
2019-02-14 20:07:31 -0500

    Updated testing conditions for test_read

--- test_read_TSH.py
+++ test_read_TSH.py
@@ -1,32 +1,20 @@
 import pytest
-import numpy
 from read_TSH import diagnosis
-
-raw_data = open("test_data.txt")
-data_list = raw_data.readlines()
-num_patient = int(len(data_list)/4)
-lab_vals = []
-
-# Create a list to iterate through
-iterate = numpy.linspace(0, 4*num_patient-4, num_patient)
-iterate = iterate.astype(int)
-
-# Seperate name and lab_vals into indiviudal data lists
-for i, c in enumerate(iterate):
-    lab_vals.append(data_list[c+3])
 
 
 @pytest.mark.parametrize("patient,expected", [
-    (lab_vals, ["normal thyroid function",
-                "hyperthyroidism",
-                "normal thyroid function",
-                "hypothyroidism",
-                "hyperthyroidism",
-                "normal thyroid function",
-                "hypothyroidism",
-                "hyperthyroidism",
-                "hypothyroidism",
-                "hyperthyroidism"]),
+    (["TSH,3.5,3.6,1.8,2.8,1.9,3.4,3,3.6,3,4"], ["normal thyroid function"]),
+    (["TSH,2.7,1.4,2.5,3.1,0.4,1.8,3.1,3,3.8,0.9,2.3"], ["hyperthyroidism"]),
+    (["TSH,2.5,1.1,1.3,2.7,1.9,2.6,3.5,1,1.4"], ["normal thyroid function"]),
+    (["TSH,6.3,6.7,6.3,7.6,2.1,6.9,7.1,4.1,7.2,3.5,2.9"], ["hypothyroidism"]),
+    (["TSH,2.8,2.5,0.7,3.8,2.7,0.2,2.9,1.5,0.8,2.4"], ["hyperthyroidism"]),
+    (["TSH,2.4,2.4,3.5,1.1,3,3.9,2,3.7,2.1,3.9"], ["normal thyroid function"]),
+    (["TSH,2.7,5.2,4.5,3.3,5.8,2.4,5.3,4.2,2.5,5.2,4"], ["hypothyroidism"]),
+    (["TSH,0.6,3.5,0.2,3.7,1.1,0.2,3.5,2.2,1,0.6,3.5"], ["hyperthyroidism"]),
+    (["TSH,3.1,4.5,3.5,3.6,5.6,4.8,4.3,5.7,4.2,2.4,5.5"], ["hypothyroidism"]),
+    (["TSH,2,2.6,2.4,2.2,1,1.4,0.2,0.5,2,2.3,0.2"], ["hyperthyroidism"]),
+    (["2,3,4,3,2,2,3.4", "TSH,  0.8,1.0,1.1,1.0,2,3,3"],
+        ["normal thyroid function", "hyperthyroidism"]),
 ])
 def test_diagnosis(patient, expected):
     """ Main testing modules for the diagnosis module.
